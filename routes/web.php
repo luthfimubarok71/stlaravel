65e1b10e42ce665ebfb040b879be8f53953a6e26
Luthfi <luthfimubarok71@gmail.com>
2025-08-28 12:55:46 +0700

    change the fake model with eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    $post = Post::find($slug);
-    if (!$post) abort(404);
+Route::get('/posts/{id}', function ($id) {
+    $post = Post::find($id);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
