e495110be151bac1291bf71809cea46bfd0aad72
Luthfi <luthfimubarok71@gmail.com>
2025-08-29 14:41:31 +0700

    add relationship in model User and Post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,8 @@
                 <h2 class="mb-1 font-bold text-3xl tracking-tight text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post['author_id'] }}</a> | 27 Agustus 2025
+                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 27
+                Agustus 2025
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
             <a class="text-blue-500 hover:underline font-medium" href="/posts/{{ $post['slug'] }}">Read More &raquo;</a>
