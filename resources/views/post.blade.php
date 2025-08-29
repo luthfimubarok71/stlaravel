9fe8109670ab130f8e4a349354e4047cf497a9bb
Luthfi <luthfimubarok71@gmail.com>
2025-08-29 15:19:06 +0700

    change author route from id to username

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,8 @@
     <article class="py-8 max-w-screen-md">
         <h2 class="mb-1 font-bold text-3xl tracking-tight text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 27 Agustus
+            <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | 27
+            Agustus
             2025
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,8 @@
                 <h2 class="mb-1 font-bold text-3xl tracking-tight text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 27
+                <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> |
+                27
                 Agustus 2025
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
