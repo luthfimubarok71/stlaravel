e831100408de02f70ad5aa197f5d46cde1887baa
Luthfi <luthfimubarok71@gmail.com>
2025-09-06 22:47:26 +0700

    delete commented code

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,4 @@
 <x-layout :title="$title">
-
-    {{-- <article class="py-8 max-w-screen-md">
-        <h2 class="mb-1 font-bold text-3xl tracking-tight text-gray-900">{{ $post['title'] }}</h2>
-        <div class="text-base text-gray-500">
-            By <a href="/authors/{{ $post->author->username }}"
-                class="hover:underline text-gray-900">{{ $post->author->name }}</a> In <a
-                href="/categories/{{ $post->category->slug }}"
-                class="hover:underline text-gray-900">{{ $post->category->name }}</a> |
-            27
-            Agustus 2025
-        </div>
-        <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
-        <a class="text-blue-500 hover:underline font-medium" href="/posts">&laquo; Back to all posts</a>
-    </article> --}}
-
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article
