16b25a3b1812c4e3c6279d81abb7d889bc60d298
Luthfi <luthfimubarok71@gmail.com>
2025-09-10 08:29:45 +0700

    update adding query scopes by query parameter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -21,5 +23,23 @@ class Post extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    #[Scope]
+    protected function filter(Builder $query, array $filters): void
+    {
+        $query->when($filters['keyword'] ?? false, function ($query, $filters) {
+            return $query->where('title', 'like', '%' . $filters . '%');
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $filters) {
+            return $query->whereHas('category', fn(Builder $query) =>
+            $query->where('slug', $filters));
+        });
+
+        $query->when($filters['author'] ?? false, function ($query, $filters) {
+            return $query->whereHas('author', fn(Builder $query) =>
+            $query->where('username', $filters));
+        });
     }
 }
