bde8a4cccaecd3d8c36b15dc2f61e071b88e1aee
Luthfi <luthfimubarok71@gmail.com>
2025-09-10 08:28:57 +0700

    update routes for query scopes by request

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,13 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    // $posts = Post::with(['category', 'author'])->latest()->get();
-    $posts = Post::latest();
+    $posts = Post::latest()->filter(request(['keyword', 'category', 'author']))->get();
 
-    if (request('keyword')) {
-        $posts->where('title', 'like', '%' . request('keyword') . '%');
-    }
-
-    return view('posts', ['title' => 'Blog', 'posts' => $posts->get()]);
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
-});
-
-Route::get('/authors/{user:username}', function (User $user) {
-    // $posts = $user->posts->load('category', 'author');
-    return view('posts', ['title' => count($user->posts) . ' Article By. ' . $user->name, 'posts' => $user->posts]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    // $posts = $category->posts->load('category', 'author');
-    return view('posts', ['title' => count($category->posts) . ' Article in Category : ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/about', function () {
